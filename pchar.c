0fc2f9fca13fc8957236994aa69aed36c0bcff34
chiro999 <r.o.g.u.e@outlook.com>
2023-09-21 16:37:05 -0700

    pchar

--- pchar.c
+++ pchar.c
@@ -1,31 +1,25 @@
 #include "monty.h"
-/**
- * f_pchar - prints the char at the top of the stack,
- * followed by a new line
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pchar(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
 
-	h = *head;
-	if (!h)
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	if (h->n > 127 || h->n < 0)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%c\n", h->n);
+void _pchar(stack_t **head, unsigned int counter)
+{
+    stack_t *new_node;
+
+    new_node = *head;
+    if (!new_node)
+    {
+        fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+        fclose(monty.file);
+        free(monty.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    if (new_node->n > 127 || new_node->n < 0)
+    {
+        fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+        fclose(monty.file);
+        free(monty.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    printf("%c\n", new_node->n);
 }
